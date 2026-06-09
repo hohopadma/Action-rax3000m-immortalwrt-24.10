1a76aa518eacacf57a8ea02e5819f4c8396e9bc7
hohopadma <162073509+hohopadma@users.noreply.github.com>
2026-06-09 15:43:09 +0800

    Update diy-part2.sh

--- diy-part2.sh
+++ diy-part2.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 # Modify default IP
-sed -i 's/192.168.1.1/192.168.123.1/g' package/base-files/files/bin/config_generate
+sed -i 's/192.168.1.1/192.168.1.2/g' package/base-files/files/bin/config_generate
 
 sudo apt install libfuse-dev
 rm -rf feeds/packages/lang/golang
